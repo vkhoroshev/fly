6da257c5c98b14645bca0c073bc3967f9ba0d2c0
Vadim Khoroshev <vadim.khoroshev@fmecgroup.com>
2021-10-28 17:11:55 +0300

    Добавить логирование событий на сервере

--- server/src/main/java/server/StartServer.java
+++ server/src/main/java/server/StartServer.java
@@ -1,7 +1,16 @@
 package server;
 
+import java.io.FileInputStream;
+import java.util.logging.LogManager;
+
 public class StartServer {
     public static void main(String[] args) {
+        try {
+            LogManager logManager = LogManager.getLogManager();
+            logManager.readConfiguration(new FileInputStream("logging.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         new Server();
     }
 }
